e0577f3b104b53441a9218abf9b18313304f466f
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2025-02-21 16:14:25 +0200

    handle crc and no-crc packets

--- tc.py
+++ tc.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import time
 from datetime import datetime
@@ -6,46 +7,53 @@ import geocoder
 
 from wialonips.protocol import PacketType, DevPacket, Protocol
 
-# HOST = "193.193.165.165"
-# PORT = 20332
 
-HOST = "127.0.0.1"
-PORT = 65432
 
-IMEI = "wips"
-PASSWORD = "wips"
 
-INCOMING_PACKET_FORMAT = "#{type}#{params}{crc}\r\n"
+if __name__ == '__main__':
 
-login_body = ";".join(["2.0", IMEI, PASSWORD, ""]).encode('ascii')
-print(login_body)
-login_packet = {
-    'type': PacketType.DEV_LOGIN.value,
-    'params': login_body.decode('ascii'),
-    'crc': DevPacket.crc_body(login_body).decode('ascii')
-}
+    if "--localhost" in sys.argv:
+        HOST = "127.0.0.1"
+        PORT = 65432
 
-login_packet = INCOMING_PACKET_FORMAT.format(**login_packet).encode('ascii')
-print(login_packet)
+    else:
+        HOST = "193.193.165.165"
+        PORT = 20332
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((HOST, PORT))  # Connect to server
+    IMEI = "wips"
+    PASSWORD = "wips"
 
-    client_socket.send(
-        login_packet
-    )  # Send data
-    print("Sent")
-    data = client_socket.recv(1024)  # Receive response
-    print("Received", data)
-    if data == b'#AL#1\r\n':
-        while True:
-            g = geocoder.ip('me')
-            lat, lon = g.latlng
-            dt = datetime.now()
-            data_body = Protocol().build_short_data_packet(dt, lat, lon, 0, 0, 100, 7)
+    INCOMING_PACKET_FORMAT = "#{type}#{params}{crc}\r\n"
 
-            client_socket.send(data_body)
-            data = client_socket.recv(1024)
-            time.sleep(3)
+    login_body = ";".join(["2.0", IMEI, PASSWORD, ""]).encode('ascii')
+    print(login_body)
+    login_packet = {
+        'type': PacketType.DEV_LOGIN.value,
+        'params': login_body.decode('ascii'),
+        'crc': DevPacket.crc_body(login_body).decode('ascii')
+    }
 
-print(f"Received from server: {data.decode()}")
+    login_packet = INCOMING_PACKET_FORMAT.format(**login_packet).encode('ascii')
+    print(login_packet)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((HOST, PORT))  # Connect to server
+
+        client_socket.send(
+            login_packet
+        )  # Send data
+        print("Sent")
+        data = client_socket.recv(1024)  # Receive response
+        print("Received", data)
+        if data == b'#AL#1\r\n':
+            while True:
+                g = geocoder.ip('me')
+                lat, lon = g.latlng
+                dt = datetime.now()
+                data_body = Protocol().build_short_data_packet(dt, lat, lon, 0, 0, 100, 7)
+
+                client_socket.send(data_body)
+                data = client_socket.recv(1024)
+                time.sleep(3)
+
+    print(f"Received from server: {data.decode()}")
